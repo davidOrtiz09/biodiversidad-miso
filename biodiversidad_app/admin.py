7d4ad8ad9a7c593cb8cd6ca834208be75172daa4
David Ortiz <davideduardo.ortiz@gmail.com>
2017-08-18 18:41:45 -0500

    Modelo de especies listo: Se creo el modelo de especies, tambien se dejo listo para poderse usar en el administrador. solo falta enlazar la llave foranea de las categorias y cuarar la direccion fisica donde se van a guardar las imagenes.

--- biodiversidad_app/admin.py
+++ biodiversidad_app/admin.py
@@ -2,5 +2,6 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from models import Species
 # Register your models here.
+admin.site.register(Species)
--- biodiversidad_app/models.py
+++ biodiversidad_app/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Species(models.Model):
+    category_id = models.CharField(max_length=150, blank=False)  # TODO: Crear la fx cuando las categorias sean creadas
+    name = models.CharField(max_length=150, blank=False)
+    picture = models.ImageField(upload_to='images', null=False)
+    short_description = models.CharField(max_length=150, blank=False)
+    long_description = models.CharField(max_length=300, blank=False)
+    scientific_name = models.CharField(max_length=150, blank=False)
+    taxonomic_classification = models.CharField(max_length=150, blank=False)
+
+
+
+
