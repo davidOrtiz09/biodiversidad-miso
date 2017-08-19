6f22968cafb1105b932b05297c2a014b66e05578
Juan Cifuentes <jc.cifuentes@uniandes.edu.co>
2017-08-18 22:56:14 -0500

    Creacion modelo Comment, pendiente FK de species, al hacer pull la complemento

--- biodiversidad_app/models.py
+++ biodiversidad_app/models.py
@@ -3,16 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
-class Species(models.Model):
-    category_id = models.CharField(max_length=150, blank=False)  # TODO: Crear la fx cuando las categorias sean creadas
-    name = models.CharField(max_length=150, blank=False)
-    picture = models.ImageField(upload_to='images', null=False)
-    short_description = models.CharField(max_length=150, blank=False)
-    long_description = models.CharField(max_length=300, blank=False)
-    scientific_name = models.CharField(max_length=150, blank=False)
-    taxonomic_classification = models.CharField(max_length=150, blank=False)
+# Create your models here.
 
 
-
+class Comment(models.Model):
+    email= models.CharField(max_length=150)
+    comment = models.CharField(max_length=1000, blank=True)
 
