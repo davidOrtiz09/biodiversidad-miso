897a4dcf15ff8f42e616808ca429484bdba5e58f
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-08-20 22:31:27 -0500

    Filtros por categorías en las especies

--- biodiversidad_app/admin.py
+++ biodiversidad_app/admin.py
@@ -15,7 +15,7 @@ class SpeciesAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'scientific_name')
     list_display_links = ('id', 'name', 'scientific_name')
     search_fields = ('name', 'scientific_name')
-    # list_filter = ('fk_category', )
+    list_filter = ('fk_category', )
     inlines = (CommentsInline, )
 
 
