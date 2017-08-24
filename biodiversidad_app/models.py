9d91892d8e89f04bb2c48ef21620278c4059dd84
Juan Cifuentes <jc.cifuentes@uniandes.edu.co>
2017-08-24 09:34:08 -0500

    revertir

--- biodiversidad_app/models.py
+++ biodiversidad_app/models.py
@@ -82,39 +82,3 @@ class AppUser(models.Model):
 
     def __str__(self):
         return '{0} {1}'.format(self.first_name, self.last_name)
-
-
-class UserForm(ModelForm):
-
-    username = forms.CharField(max_length=50)
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(max_length=20)
-    email = forms.EmalField()
-    password = forms.CharField(widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        model = User
-        fields = ['username','first_name','last_name','email','password','password2']
-
-    def clean_username(self):
-
-        username = self.cleaned_data['username']
-        if User.objects.filter(username=username):
-            raise forms.ValidationError('Nombre de usuario ya registrado. ')
-        return username
-
-    def clean_email(self):
-
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email):
-            raise forms.ValidationError('Yaexsiste un email igual registrado. ')
-        return email
-
-    def clean_password2(self):
-
-        password = self.cleaned_data['password']
-        password2 = self.cleaned_data['password2']
-        if password != password2:
-            raise forms.ValidationError('Las claves no coinciden. ')
-        return password2
