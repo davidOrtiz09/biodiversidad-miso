69433afb074db5f1f077a5b05456a2039c55f003
David Ortiz <davideduardo.ortiz@gmail.com>
2017-08-24 21:28:52 -0500

    Construccion inicial login: Se emepezo la contruccion del login , ya se hizo las platinas y la vista. Solo falta conectar con logica.

--- biodiversidad_app/urls.py
+++ biodiversidad_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='index')
+    url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^login/$', views.login_view, name='login'),
 ]
--- biodiversidad_app/views.py
+++ biodiversidad_app/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.contrib.auth import authenticate, login
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from .models import Species
 
 
@@ -10,3 +13,22 @@ class Index(View):
         species_list = Species.objects.all()
         context = {'species_list': species_list}
         return render(request, 'biodiversidad_app/index.html', context)
+
+
+def login_view(request):
+
+    if request.user.is_authenticated():
+        return redirect(reverse('biodiversidad:index'))
+
+    mensaje = ''
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('biodiversidad:index'))
+        else:
+            mensaje = 'Nombre de usuario o clave invalido'
+
+    return render(request, 'biodiversidad_app/_elements/_log-in.html', {'mensaje': mensaje})
