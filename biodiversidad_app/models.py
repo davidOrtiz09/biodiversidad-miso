451aee9013c5f7b3aa4f21fedb2a2fab6472c7d8
Juan Cifuentes <jc.cifuentes@uniandes.edu.co>
2017-08-25 22:33:02 -0500

    Registro listo, pero presenta un problema al renderizar el index

--- biodiversidad_app/models.py
+++ biodiversidad_app/models.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib.auth.forms import forms
+from django.forms import ModelForm
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.contrib.auth.models import User
@@ -79,4 +81,39 @@ class AppUser(models.Model):
     def __str__(self):
         return '{0} {1}'.format(self.first_name, self.last_name)
 
+
+class UserForm(ModelForm):
+
+    username = forms.CharField(max_length=50)
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ['username','first_name','last_name','email','password','password2']
+
+    def clean_username(self):
+
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username):
+            raise forms.ValidationError('Nombre de usuario ya registrado. ')
+        return username
+
+    def clean_email(self):
+
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email):
+            raise forms.ValidationError('Ya exsiste un email igual registrado. ')
+        return email
+
+    def clean_password2(self):
+
+        password = self.cleaned_data['password']
+        password2 = self.cleaned_data['password2']
+        if password != password2:
+            raise forms.ValidationError('Las claves no coinciden.')
+        return password2
 
--- biodiversidad_app/views.py
+++ biodiversidad_app/views.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 from django.views.generic import View
 from django.shortcuts import render
 from .models import Species
+from .models import UserForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 class Index(View):
@@ -10,3 +14,29 @@ class Index(View):
         species_list = Species.objects.all()
         context = {'species_list': species_list}
         return render(request, 'biodiversidad_app/index.html', context)
+
+
+
+def add_user_view(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            username = cleaned_data.get('username')
+            first_name = cleaned_data.get('first_name')
+            last_name = cleaned_data.get('last_name')
+            password = cleaned_data.get('password')
+            email = cleaned_data.get('email')
+
+            user_model = User.objects.create_user(username=username, password=password)
+            user_model.first_name = first_name
+            user_model.last_name = last_name
+            user_model.email = email
+            user_model.save()
+            return HttpResponseRedirect(reverse('biodiversidad:index'))
+    else:
+        form = UserForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'biodiversidad_app/_forms/user_registration.html', context)
