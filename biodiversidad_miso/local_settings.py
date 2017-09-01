c8c3ab4db891bba700b4bb21a007cb46aa7f5995
Alfonso Ardila <alfyes@hotmail.com>
2017-09-01 15:32:58 -0500

    Se deja la configuración de la BD con la de despliegue.

--- biodiversidad_miso/local_settings.py
+++ biodiversidad_miso/local_settings.py
@@ -3,12 +3,12 @@ import os
 import dj_database_url
 
 # Please use this dict when deploying to heroku
-''''''
+
 DATABASE_DICT = dj_database_url.config(default=os.environ.get('DATABASE_URL', ''))
-''''''
+
 # Please use this dict for locale tests
 
-
+'''
 DATABASE_DICT = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'biodiversidad_db',
@@ -17,6 +17,6 @@ DATABASE_DICT = {
     'HOST': '127.0.0.1',
     'PORT': '5432',
 }
-
+'''
 
 
