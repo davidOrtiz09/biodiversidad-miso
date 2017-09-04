636c597f9b3531b213e12330d5ca4d4a8d3d01bd
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-09-03 22:28:52 -0500

    Se corrige error con las urls

--- biodiversidad_app/urls.py
+++ biodiversidad_app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login$', views.Login.as_view(), name='login'),
     url(r'^logout$', views.Logout.as_view(), name='logout'),
-    url(r'^especie/(?P<species_id>\d+)$', views.specie_view, name='verEspecie'),
-    url(r'^registrar-usuario$', views.add_user_view, name='addUser'),
-    url(r'^actualizar-usuario$', views.update_user_view, name='updateUser'),
-    url(r'^comentar/(?P<species_id>\d+)$', views.add_comment, name='addComment')
+    url(r'^especie/(\d+)/$', views.specie_view, name='verEspecie'),
+    url(r'^registrar-usuario/$', views.add_user_view, name='addUser'),
+    url(r'^actualizar-usuario/$', views.update_user_view, name='updateUser'),
+    url(r'^comentar/(?P<species_id>\d+)/$', views.add_comment, name='addComment')
 ]
